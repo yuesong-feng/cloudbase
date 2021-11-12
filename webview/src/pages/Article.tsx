8864ff31a7e50ab50cf029fd83a13724f5be9bd6
yuesong-feng <yuesong-feng@foxmail.com>
2021-11-12 15:30:44 +0800

    update

--- webview/src/pages/Article.tsx
+++ webview/src/pages/Article.tsx
@@ -1,39 +1,33 @@
 import React from "react";
 import "./Article.less";
 import defaultImg from "../images/defaultImg.jpeg";
-import E from 'wangeditor'
-let editor: any = null
-
 interface ArticleProps {
   history: any;
 }
 export default class Article extends React.Component<ArticleProps> {
   constructor(props: ArticleProps | Readonly<ArticleProps>) {
     super(props);
+
   }
   componentDidMount(){
-    editor = new E('#toolbar-container', '#text-container')
-    editor.create()
-    editor.txt.html(`<h1 id="t2l9d"><font color="#c24f4a">123</font></h1><p><font color="#c24f4a"><b>243</b><br/></font></p><p><font color="#c24f4a"><font face="黑体">1分钱微风</font><b><br/></b></font></p><p><font color="#c24f4a"></font><span style="font-size: 14px;">😛</span><font face="黑体"><br/></font></p><table border="0" width="100%" cellpadding="0" cellspacing="0"><tbody><tr><th></th><th></th><th></th><th></th><th></th></tr><tr><td></td><td></td><td></td><td></td><td></td></tr><tr><td>阿俄法</td><td></td><td></td><td></td><td></td></tr><tr><td></td><td></td><td></td><td></td><td></td></tr><tr><td></td><td></td><td></td><td></td><td></td></tr></tbody></table><h1 id="t2l9d"><font color="#c24f4a">123</font></h1><p><font color="#c24f4a"><b>243</b><br/></font></p><p><font color="#c24f4a"><font face="黑体">1分钱微风</font><b><br/></b></font></p><p><font color="#c24f4a"></font><span style="font-size: 14px;">😛</span><font face="黑体"><br/></font></p><table border="0" width="100%" cellpadding="0" cellspacing="0"><tbody><tr><th></th><th></th><th></th><th></th><th></th></tr><tr><td></td><td></td><td></td><td></td><td></td></tr><tr><td>阿俄法</td><td></td><td></td><td></td><td></td></tr><tr><td></td><td></td><td></td><td></td><td></td></tr><tr><td></td><td></td><td></td><td></td><td></td></tr></tbody></table><h1 id="t2l9d"><font color="#c24f4a">123</font></h1><p><font color="#c24f4a"><b>243</b><br/></font></p><p><font color="#c24f4a"><font face="黑体">1分钱微风</font><b><br/></b></font></p><p><font color="#c24f4a"></font><span style="font-size: 14px;">😛</span><font face="黑体"><br/></font></p><table border="0" width="100%" cellpadding="0" cellspacing="0"><tbody><tr><th></th><th></th><th></th><th></th><th></th></tr><tr><td></td><td></td><td></td><td></td><td></td></tr><tr><td>阿俄法</td><td></td><td></td><td></td><td></td></tr><tr><td></td><td></td><td></td><td></td><td></td></tr><tr><td></td><td></td><td></td><td></td><td></td></tr></tbody></table>`)
-    editor.disable()
   }
   componentWillUnmount(){
-    editor.destroy()
   }
 
-  getParam(key: string) {
-    let query = this.props.history.location.search.substring(1);
-    let paramList = query.split("&");
-    for (let i = 0; i < paramList.length; i++) {
-      let key_value = paramList[i].split("=");
-      if (key_value[0] === key) {
-        return decodeURIComponent(key_value[1]);
-      }
-    }
-    return "";
-  }
+  // getParam(key: string) {
+  //   let query = this.props.history.location.search.substring(1);
+  //   let paramList = query.split("&");
+  //   for (let i = 0; i < paramList.length; i++) {
+  //     let key_value = paramList[i].split("=");
+  //     if (key_value[0] === key) {
+  //       return decodeURIComponent(key_value[1]);
+  //     }
+  //   }
+  //   return "";
+  // }
   render() {
     return (
+      
       <div className="articlePage">
         <div className="articleTopBar">
           <button
@@ -45,10 +39,8 @@ export default class Article extends React.Component<ArticleProps> {
 
         <div className="articleCard">
           <img src={defaultImg} className="articleImg" />
-          <div id="toolbar-container"></div>
-          <div id="text-container"></div>
         </div>
-
+        
       </div>
     );
   }
--- webview/src/pages/EditArticle.tsx
+++ webview/src/pages/EditArticle.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./EditArticle.less";
 import defaultImg from "../images/defaultImg.jpeg";
-import E from "wangeditor";
-let editor1: any = null;
-let editor2: any = null;
+
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 interface EditArticleProps {
   history?: any;
@@ -13,52 +13,17 @@ export default class Article extends React.Component<EditArticleProps> {
     super(props);
   }
   componentDidMount() {
-    editor2 = new E("#text-display");
-    editor2.create();
-    editor2.disable();
-
-    editor1 = new E("#toolbar-container", "#text-container");
-    editor1.config.onchange = (newHtml: any) => {
-      editor2.txt.html(newHtml)
-    };
-    editor1.create();
   }
   componentWillUnmount() {
-    editor1.destroy();
-    editor2.destroy();
-  }
-
-  getParam(key: string) {
-    let query = this.props.history.location.search.substring(1);
-    let paramList = query.split("&");
-    for (let i = 0; i < paramList.length; i++) {
-      let key_value = paramList[i].split("=");
-      if (key_value[0] === key) {
-        return decodeURIComponent(key_value[1]);
-      }
-    }
-    return "";
   }
   render() {
     return (
       <div className="articlePage">
-        <div className="articleTopBar">
-          {/* <button
-            onClick={() => {
-              this.props.history.goBack();
-            }}
-          >back</button> */}
-        </div>
+        <div className="articleTopBar"></div>
 
         <div className="articleCard">
           <img src={defaultImg} className="articleImg" />
-          <div className="editLeft">
-            <div id="toolbar-container"></div>
-            <div id="text-container"></div>
-          </div>
-          <div className="editRight">
-            <div id="text-display"></div>
-          </div>
+          <ReactQuill className="editor" />
         </div>
       </div>
     );
